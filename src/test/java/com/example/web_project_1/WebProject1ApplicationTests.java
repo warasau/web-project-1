9a0151d0c1c9023779be738787d0e470621c7829
warasau <galaksylive@gmail.com>
2025-12-04 21:18:53 +0300

    Version 8: - lab 6 - CI v2

---
+++ src/test/java/com/example/web_project_1/WebProject1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.web_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
